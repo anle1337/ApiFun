6391fe58a649f13da783f2df862a28f341185744
An Le <asble@ucdavis.edu>
2016-01-28 20:24:12 -0800

    bamboo edits

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -5,10 +5,10 @@ layout 'with_back_button'
     @bamboo = Person.new.user_details
     @person = Person.new
 
-    @json = HTTParty.get("https://api.bamboohr.com/api/gateway.php/mechina/v1/employees/#{rand(40411..40510)}?fields=fullName1,firstName,lastNameaddress1,city,state,stateCode,country,workEmail,workPhone", {:basic_auth => {:username => ENV['BAMBOO_API_KEY'] }, :headers => {"Accept" => "application/json" }})
+    @json = HTTParty.get("https://api.bamboohr.com/api/gateway.php/mechina/v1/employees/#{rand(40411..40510)}?fields=fullName1,firstName,lastName,address1,city,state,stateCode,country,workEmail,workPhone", {:basic_auth => {:username => ENV['BAMBOO_API_KEY'] }, :headers => {"Accept" => "application/json" }})
     @json = ActiveSupport::JSON.decode(@json.body)
 
-    @last = HTTParty.get("https://api.bamboohr.com/api/gateway.php/mechina/v1/employees/#{@bamboo.last_employee_added}?fields=fullName1,firstName,lastNameaddress1,city,state,stateCode,country,workEmail,workPhone", {:basic_auth => {:username => ENV['BAMBOO_API_KEY'] }, :headers => {"Accept" => "application/json" }})
+    @last = HTTParty.get("https://api.bamboohr.com/api/gateway.php/mechina/v1/employees/#{@bamboo.last_employee_added}?fields=fullName1,firstName,lastName,address1,city,state,stateCode,country,workEmail,workPhone", {:basic_auth => {:username => ENV['BAMBOO_API_KEY'] }, :headers => {"Accept" => "application/json" }})
     @last = ActiveSupport::JSON.decode(@last.body)
     
 
